cc601410d7223a4096b44c99969e223afdaaa714
Andrew Kharchyshyn <haawaplus@gmail.com>
2017-05-14 18:21:27 +0300

    Carthage compatibility

---
+++ DrawingView/DrawingView.h
@@ -0,0 +1,19 @@
+//
+//  DrawingView.h
+//  DrawingView
+//
+//  Created by Andriy K. on 5/14/17.
+//  Copyright © 2017 Stefano Acerbetti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DrawingView.
+FOUNDATION_EXPORT double DrawingViewVersionNumber;
+
+//! Project version string for DrawingView.
+FOUNDATION_EXPORT const unsigned char DrawingViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrawingView/PublicHeader.h>
+
+
